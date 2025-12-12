1587940ab046597d7f05492b9cc196aed82358ac
DzahwanMalik <malikdzahwan22@gmail.com>
2025-12-12 17:12:56 +0700

    add pool to fix max connection error

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "mysql",
-  dialectModule: mysql2,
-});
-            
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectModule: mysql2,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
+
 export default db;
